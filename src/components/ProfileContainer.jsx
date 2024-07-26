b69459d3c578999928874be1d9c0fed16a086fb0
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-07-26 20:50:16 +0200

    test upload image

--- src/components/ProfileContainer.jsx
+++ src/components/ProfileContainer.jsx
@@ -4,17 +4,20 @@ import { getProfileUrl } from '../fetchUrls';
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from './contexts/UserContextProvider';
 
-
 function ProfilePage({ id }) {
     // l'id lo ricevo direttamente dal componente padre    
     const [user, setUser] = useState(null)
+    // stato per rendere visibile il form per cambiare immagine
+    const [editMode, setEditMode] = useState(false)
+    // stato per archiviare la nuova immagine e gestire il form soprastante
+    const [fd, setFd] = useState(new FormData())
 
     // chiave per la fetch
     const apiKey = process.env.REACT_APP_APIKEY;
 
     const { selectedUser, setSelectedUser } = useContext(UserContext)
 
-    // funzione che effettua la fetch
+    // funzione che effettua la fetch per recuperare i dati del profilo in base all'id
     const getProfileData = async () => {
         const resp = await fetch(getProfileUrl + '/' + id, {
             method: 'GET',
@@ -28,16 +31,51 @@ function ProfilePage({ id }) {
         setUser(data)
     }
 
+    // aggiornamento dei dati del profilo su variazione dell'id
     useEffect(() => {
         id && id != selectedUser?._id ? getProfileData() : setUser(selectedUser)
     }, [id, selectedUser])
+
+    const editPhoto = function () {
+        setEditMode(!editMode)
+    }
+
+    const changePhoto = async (ev) => {
+        ev?.preventDefault()
+        let resp = await fetch( `${getProfileUrl}/${id}/picture`,
+            {
+                method: "POST",
+                body: fd, //non serve JSON.stringify
+                headers: {
+                    Authorization: apiKey
+                },
+            }
+        )
+        if(resp.ok) {
+            console.log('image changed')
+        }
+        else{
+            console.log('error')
+        } 
+    }
+
+
+    const handleFile = (ev) => {
+        setFd((prev) => {
+            //per cambiare i formData, bisogna "appendere" una nuova coppia chiave/valore, usando il metodo .append()
+            prev.delete("post") //ricordatevi di svuotare il FormData prima :)
+            prev.append("post", ev.target.files[0]) //L'API richiede un "nome" diverso per ogni rotta, per caricare un'immagine ad un post, nel form data andra' inserito un valore con nome "post"
+            return prev
+        })
+    }
 
     return (
         // selectedUser && mi serve perchè così evito di andare in errore perchè la roba della fetch non è ancora arrivata
         // renderizzo il componente solo quando ci sono anche i dati al suo interno
         user && <Container id='profileContainer'>
             <div id='upperSection'>
-                <Button id='editPhoto'>
+                <Button id='editPhoto'
+                    onClick={() => { editPhoto() }}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#0966c2" className="bi bi-camera-fill" viewBox="0 0 16 16">
                         <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
                         <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0" />
@@ -50,6 +88,13 @@ function ProfilePage({ id }) {
                         <circle cx="80" cy="80" r="79" fill="transparent" stroke="white" strokeWidth="5"></circle>
                     </svg>
                     <img src={user.image} alt="" />
+                    {/* FORM UPLOAD IMMAGINE */}
+                    {editMode &&
+                        <form className='imgForm'>
+                            <input type="file" onChange={handleFile} />
+                            <button onClick={() => {changePhoto()}}>SEND</button>
+                        </form>
+                    }
                 </div>
             </div>
             <div id='lowerSection'>
